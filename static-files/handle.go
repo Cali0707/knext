407d9a47e82ac925ad398a77d8d58d7862434135
Calum Murray <cmurray@redhat.com>
2024-03-28 15:55:19 -0400

    feat: the static file server works
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- static-files/handle.go
+++ static-files/handle.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"embed"
 	"fmt"
+	"io"
+	"io/fs"
+	"net/http"
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/cloudevents/sdk-go/v2/event"
@@ -15,51 +18,66 @@ type StaticFileRequest struct {
 
 // content holds the static web server content
 //
-//go:embed public/*
+//go:embed static/*
 var content embed.FS
 
 // Handle an event.
-func Handle(ctx context.Context, e event.Event) (*event.Event, error) {
-	/*
-	 * YOUR CODE HERE
-	 *
-	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
-	 */
+func Handle(_ context.Context, e event.Event) (*event.Event, error) {
 
 	req := &StaticFileRequest{}
 	err := e.DataAs(req)
 	if err != nil {
+		fmt.Printf("%s\n", err.Error())
+		fmt.Printf("%v\n", req)
 		return nil, err
 	}
 
-	file, err := content.ReadFile(req.FilePath)
+	file, err := content.Open(fmt.Sprintf("static/%s", req.FilePath))
 	if err != nil {
+		fmt.Printf("%s\n", err.Error())
+		return nil, err
+	}
+
+	content, contentType, err := readFile(file)
+	if err != nil {
+		fmt.Printf("%s\n", err.Error())
 		return nil, err
 	}
 
 	event := cloudevents.NewEvent()
 	event.SetSource("knext/staticserver")
-	event.SetType("knext.static.response")
-	event.SetData("text/html", string(file))
+	event.SetType("knext.static.success")
+	err = event.SetData(contentType, content)
 
 	fmt.Println(event) // echo to local output
-	return &event, nil // echo to caller
+	return &event, err // echo to caller
 }
 
-/*
-Other supported function signatures:
+func readFile(file fs.File) ([]byte, string, error) {
+	var size int
+	if info, err := file.Stat(); err == nil {
+		size64 := info.Size()
+		if int64(int(size64)) == size64 {
+			size = int(size64)
+		}
+	}
 
-	Handle()
-	Handle() error
-	Handle(context.Context)
-	Handle(context.Context) error
-	Handle(event.Event)
-	Handle(event.Event) error
-	Handle(context.Context, event.Event)
-	Handle(context.Context, event.Event) error
-	Handle(event.Event) *event.Event
-	Handle(event.Event) (*event.Event, error)
-	Handle(context.Context, event.Event) *event.Event
-	Handle(context.Context, event.Event) (*event.Event, error)
+	var contentType string
+	data := make([]byte, 0, size+1)
+	for {
+		if len(data) >= cap(data) {
+			d := append(data[:cap(data)], 0)
+			data = d[:len(data)]
+		}
+		n, err := file.Read(data[len(data):cap(data)])
+		data = data[:len(data)+n]
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+				contentType = http.DetectContentType(data)
+			}
+			return data, contentType, err
+		}
+	}
 
-*/
+}
